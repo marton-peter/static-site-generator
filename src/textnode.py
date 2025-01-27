ce4b773a1a71167d432c8aa806622548b8763340
marton-peter <marton.peter85@gmail.com>
2025-01-27 18:28:40 +0100

    TextNode

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "Normal text"
+    BOLD = "Bold text"
+    ITALIC = "Italic text"
+    CODE = "Code text"
+    LINK = "Link"
+    IMAGE = "Image"
+
+class TextNode ():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if (self.text == other.text and
+        self.text_type == other.text_type and
+        self.url == other.url
+        ):
+            return True
+        else: 
+            return False
+        
+    def __repr__(self):
+        return f"TextNode({self.text.upper()}, {self.text_type.upper()}, {self.url.upper()})"
